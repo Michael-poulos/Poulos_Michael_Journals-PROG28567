6583b735567fe38365d30d3214991ef16bb076a5
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-09-10 16:19:32 -0400

    Task 1 commit (correction)
    Disregard the previous commit. I'm still getting back into the rhythm of Github. This commit properly includes the Unity project with it's GitIgnore files correctly in place. As per the instructions, I spent only a little over 45 minutes attempting the first task. More details in journal submission: https://docs.google.com/document/d/1pktMaFCRl_4ue16w6mpt3OqS8bWjnuqeyx0a7GZC2FU/edit?usp=sharing

---
+++ Week_1_Journal_Vector_Exploration_Michael_Poulos/Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SquareSpawner
+{
+    public Vector2(float x, float y);
+
+
+    
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color = Color.white, float duration = 0.0f, bool depthTest = true);
+
+
+
+
+    public static Vector3 mousePosition;
+
+
+
+
+    public static bool GetMouseButtonDown(int button);
+
+
+
+
+
+    public static Vector2 mouseScrollDelta;
+
+
+
+
+
+    public Vector3 ScreenToWorldPoint(Vector3 position);
+
+}
