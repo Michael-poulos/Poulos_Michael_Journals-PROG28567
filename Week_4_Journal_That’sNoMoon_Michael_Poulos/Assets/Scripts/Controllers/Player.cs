ec5b5b202b506c4fe65f6a12a599847fb20a0a06
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-09-26 19:36:58 -0400

    Edited Player.cs

---
+++ Week_4_Journal_That’sNoMoon_Michael_Poulos/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    
+    
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        // draw a 5-unit white line from the origin for 2.5 seconds
+        Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.red);
+
+
+        // public void EnemyRadar(float radius, int circlePoints 
+        //{
+        //  if ()
+        //  {
+        //    return ();
+        //   }
+
+        //  }
+    }
+}
