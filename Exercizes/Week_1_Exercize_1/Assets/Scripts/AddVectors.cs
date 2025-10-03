13d754e7776750cd67b21577eef861a66390a9fd
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-10-03 17:01:32 -0400

    Progress on Ex 1

--- Exercizes/Week_1_Exercize_1/Assets/Scripts/AddVectors.cs
+++ Exercizes/Week_1_Exercize_1/Assets/Scripts/AddVectors.cs
@@ -10,6 +10,25 @@ public class AddVectors : MonoBehaviour
         // Exercise: Visualizing Vector Addition
         // Create a new Vector2 called "rPlusB" which adds rTransform's position to bTransform's position.​
 
+        Vector2 rTransform = new Vector2(0, 1);
+        Vector2 bTransform = new Vector2(1, 7);
+        Vector2 rPlusB = rTransform + bTransform;
+
+        if (Input.GetKeyDown("B"))
+        {
+            Debug.DrawLine(transform.position, bTransform, Color.blue);
+        }
+        if (Input.GetKeyDown("R"))
+        {
+            Debug.DrawLine(transform.position, bTransform, Color.red);
+        }
+        if (Input.GetKeyDown("R+B"))
+        {
+            Debug.DrawLine(transform.position, bTransform, Color.magenta);
+        }
+
+
+
         // When the B key is held down, draw a blue line from the origin to bTransform's position.​
         // When the R key is held down, draw a red line from the origin to rTransform's position.​
         // When the R and B keys are both held down, draw a magenta from the origin to rPlusB.​
@@ -17,5 +36,8 @@ public class AddVectors : MonoBehaviour
         // Exercise: Calculating Magnitude
         // Calculate the magnitude of bPlusR using the mathematical formula for calculating the length of a vector – Pythagorean Theroem.​
         // Output the magnitude to the console using either Debug.Log or print.
+
+
+
     }
 }
