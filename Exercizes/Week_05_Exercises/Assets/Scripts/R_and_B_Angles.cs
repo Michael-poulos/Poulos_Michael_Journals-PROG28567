5b5b8f23ddb7762ec7200967603c0017194c1f15
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-10-07 14:30:19 -0400

    Added Week 5 Exercise to Repo

---
+++ Exercizes/Week_05_Exercises/Assets/Scripts/R_and_B_Angles.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+using UnityEngine;
+
+public class R_and_B_Angles : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float redAngle = 30;
+        float blueAngle = 60;
+
+        //redVector = CalculateVectorFrontAngle(redAngle);
+      //  blueVector = CalculateVectorFrontAngle(blueAngle);
+
+      //  Debug.DrawLine(Vector3.zero, redVector, Color.red);
+      //  Debug.DrawLine(Vector3.zero, blueVector, Color.red);
+
+
+    }
+}
