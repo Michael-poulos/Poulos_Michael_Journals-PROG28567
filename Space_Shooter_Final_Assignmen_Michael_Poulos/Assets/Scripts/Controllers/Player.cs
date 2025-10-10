de407aab4076f73cd9c3ee6580f22d352fc199d2
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-10-10 16:47:17 -0400

    Added Space shooter package and added player movement.

--- Space_Shooter_Final_Assignmen_Michael_Poulos/Assets/Scripts/Controllers/Player.cs
+++ Space_Shooter_Final_Assignmen_Michael_Poulos/Assets/Scripts/Controllers/Player.cs
@@ -1,15 +1,77 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    
-    // Update is called once per frame
+    public Transform bombTransform;
+
+    public float accelerationTime, decelerationTime;
+    public float maxSpeed;
+    private float acceleration, deceleration;
+    private Vector3 velocity;
+
+
+    private void Start()
+    {
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
+    }
+
+
     void Update()
     {
 
+        Vector2 playerInput = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerInput += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerInput += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerInput += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerInput += Vector2.right;
+        }
+
+        if (playerInput.magnitude > 0)
+        {
+            velocity += (Vector3)playerInput.normalized * acceleration * Time.deltaTime;
+                
+            if (velocity.magnitude > maxSpeed)
+                {
+                    velocity = velocity.normalized * maxSpeed;
+                }
+        }
+        else
+        {
+            Vector3 changeInVelocity = velocity.normalized * deceleration * Time.deltaTime;
+             if (changeInVelocity.magnitude > velocity.magnitude)
+             {
+                    velocity = Vector3.zero;
+             }
+             else
+             {
+                    velocity -= changeInVelocity;
+             }
+
+        }
+
+            transform.position += velocity * Time.deltaTime;
+        }
     }
-}
+
+
+    
