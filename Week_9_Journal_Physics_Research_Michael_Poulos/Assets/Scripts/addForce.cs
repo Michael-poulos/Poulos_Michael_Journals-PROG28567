f3dc097f302f6c4354e7370ab9a3702a2e3c82d9
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-11-10 22:28:34 -0500

    Added Week 9 Journal and made a few edits

--- Week_9_Journal_Physics_Research_Michael_Poulos/Assets/Scripts/addForce.cs
+++ Week_9_Journal_Physics_Research_Michael_Poulos/Assets/Scripts/addForce.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FollowMouse : MonoBehaviour
+public class addForce : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,6 +11,6 @@ public class FollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position 
+        AddForceAtPosition(Vector2 3,0, Vector2 0,0, ForceMode2D mode);
     }
 }
