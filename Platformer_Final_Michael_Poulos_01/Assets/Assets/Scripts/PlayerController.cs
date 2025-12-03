410518d89fddcd4f5ac6459b9af8ae6d56d783fa
Michael-poulos <101215908+Michael-poulos@users.noreply.github.com>
2025-12-03 15:20:59 -0500

    push

---
+++ Platformer_Final_Michael_Poulos_01/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum FacingDirection
+    {
+        left = -1, right = 1
+    }
+
+    public enum CharacterState
+    { 
+        Idle, Walking, Jumping, Dead
+    }
+
+    private CharacterState state = CharacterState.Idle;
+
+    [SerializeField] private Rigidbody2D body2D;
+
+    [Header("Walk Properties")]
+    public float maxSpeed = 5f;
+    public float accelerationTime = 0.5f;
+    public float decelerationTime = 0.25f;
+
+    [Header("Jump Properties")]
+    public float apexHeight = 3.5f;
+    public float apexTime = 0.5f;
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.55f;
+    public Vector2 groundCheckSize = new(0.75f, .2f); 
+
+    private Vector2 velocity;
+    private float acceleration;
+    private float deceleration;
+
+    private float gravity;
+    private float jumpVel;
+
+    private Vector2 playerInput;
+    private bool jumpPressed = false;
+
+    void Start()
+    {
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
+
+        gravity = -2 * apexHeight / (apexTime * apexTime);
+        jumpVel = 2 * apexHeight / apexTime;
+
+        body2D.gravityScale = 0;
+    }
+
+    void Update()
+    {
+        playerInput = new()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetButtonDown("Jump") ? 1 : 0
+        };
+
+        if (playerInput.y == 1) jumpPressed = true;
+    }
+
+    private void FixedUpdate()
+    {
+        MovementUpdate();
+    }
+
+    private void MovementUpdate()
+    {
+        ProcessWalkInput();
+        ProcessJumpInput();
+
+        body2D.linearVelocity = velocity;
+    }
+
+    /// <summary>
+    /// Modifies velocity.x based on playerInput.x.
+    /// </summary>
+    private void ProcessWalkInput()
+    {
+        if (playerInput.x != 0)
+        {
+            if (Mathf.Sign(playerInput.x) != Mathf.Sign(velocity.x)) velocity.x *= -1;
+            velocity.x += playerInput.x * acceleration * Time.fixedDeltaTime;
+
+            velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        }
+        else if (Mathf.Abs(velocity.x) > 0.005f)
+        {
+            velocity.x += -Mathf.Sign(velocity.x) * deceleration * Time.fixedDeltaTime;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+    }
+
+    /// <summary>
+    /// Modifies velocity.y based on playerInput.y.
+    /// </summary>
+    private void ProcessJumpInput()
+    {
+        if (IsGrounded() && jumpPressed)
+        {
+            velocity.y = jumpVel;
+            jumpPressed = false;
+        }
+        else if (!IsGrounded())
+        {
+            velocity.y += gravity * Time.fixedDeltaTime;
+            velocity.y = Mathf.Max(velocity.y, -jumpVel);
+            jumpPressed = false;
+        }
+        else
+            velocity.y = 0;
+    }
+
+    public bool IsWalking()
+    {
+        return playerInput.x != 0;
+    }
+    public bool IsGrounded()
+    {
+        Vector3 origin = transform.position + Vector3.down * groundCheckDistance;
+        
+        DrawGroundCheck(origin);
+
+        return Physics2D.OverlapBox(origin, groundCheckSize, 0, groundLayer);
+    }
+
+    private void DrawGroundCheck(Vector3 origin)
+    {
+        float halfW = groundCheckSize.x * 0.5f;
+        float halfH = groundCheckSize.y * 0.5f;
+
+        Debug.DrawLine(origin + new Vector3(halfW, halfH), origin + new Vector3(halfW, -halfH), Color.yellow);
+        Debug.DrawLine(origin + new Vector3(halfW, -halfH), origin + new Vector3(-halfW, -halfH), Color.yellow);
+        Debug.DrawLine(origin + new Vector3(-halfW, -halfH), origin + new Vector3(-halfW, halfH), Color.yellow);
+        Debug.DrawLine(origin + new Vector3(-halfW, halfH), origin + new Vector3(halfW, halfH), Color.yellow);
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        return (FacingDirection)playerInput.x;
+    }
+}
---
+++ Platformer_Final_Michael_Poulos_01/Assets/Assets/Scripts/PlayerVisuals.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerVisuals : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer bodyRenderer;
+    public PlayerController playerController;
+
+    private readonly int isWalkingHash = Animator.StringToHash("IsWalking");
+    private readonly int isGroundedHash = Animator.StringToHash("IsGrounded");
+    private readonly int deathStateHash = Animator.StringToHash("Death");
+
+    void Update()
+    {
+        animator.SetBool(isWalkingHash, playerController.IsWalking());
+        animator.SetBool(isGroundedHash, playerController.IsGrounded());
+
+        switch (playerController.GetFacingDirection())
+        {
+            case PlayerController.FacingDirection.left:
+                bodyRenderer.flipX = true;
+                break;
+            case PlayerController.FacingDirection.right:
+                bodyRenderer.flipX = false;
+                break;
+        }
+
+        //debug purpose only
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.Play(deathStateHash);
+        }
+    }
+}
